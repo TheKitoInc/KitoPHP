51f205e06b7ff9aeba100a9bce387133ab354f9f
TheKito <11139894+TheKito@users.noreply.github.com>
2022-05-17 23:28:25 -0300

    Update Exception.php

--- src/Kito/Exception.php
+++ src/Kito/Exception.php
@@ -1,6 +1,6 @@
 <?php
-/*
- *
+
+/**
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation; either version 3 of the License, or
@@ -10,7 +10,6 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- *
  */
 
 namespace Kito;
@@ -20,4 +19,19 @@ namespace Kito;
  */
 class Exception extends \Exception
 {
+    public function __construct(string $message = '', int  $code = 0, ?\Throwable  $previous = null)
+    {
+        if ($code === 0) {
+            $code = crc32(get_called_class());
+        }
+
+        parent::__construct($message, $code, $previous);
+    }
+
+    public static function throwException(string $message = '', int $code = 0, ?\Throwable $previous = null)
+    {
+        $class = get_called_class();
+
+        throw new $class($message, $code, $previous);
+    }
 }
