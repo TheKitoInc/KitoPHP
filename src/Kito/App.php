a5ac6df88635624d69157513e1e6158edc9d7ac0
TheKito <11139894+TheKito@users.noreply.github.com>
2022-09-15 11:25:49 -0300

    update description

--- src/Kito/App.php
+++ src/Kito/App.php
@@ -2,15 +2,16 @@
 
 declare(strict_types=1);
 /**
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or
- * (at your option) any later version.
+ * KitoApp.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
+ * php version 7.2.
+ *
+ * @category Request/Response PSR Handler
+ *
+ * @author  The Kito <thekito@blktech.com>
+ * @license http://opensource.org/licenses/gpl-license.php GNU GPL
+ *
+ * @link https://github.com/TheKito/KitoPHP
  */
 
 namespace Kito;
@@ -18,11 +19,7 @@ namespace Kito;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
 
-/**
- * Description of App.
- *
- * @author Instalacion
- */
+
 class App
 {
     private $request;
