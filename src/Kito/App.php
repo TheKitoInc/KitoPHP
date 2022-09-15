6d1b4a74ec4680693af92a60c98793a760e9407f
TheKito <11139894+TheKito@users.noreply.github.com>
2022-09-15 09:27:37 -0300

    Update App.php

--- src/Kito/App.php
+++ src/Kito/App.php
@@ -1,12 +1,22 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+declare(strict_types=1);
+/**
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
  */
 
 namespace Kito;
+
+use \Psr\Http\Message\RequestInterface;
+use \Psr\Http\Message\ResponseInterface;
 
 /**
  * Description of App.
@@ -15,46 +25,52 @@ namespace Kito;
  */
 class App
 {
-    private $psr17Factory;
+
     private $request;
     private $response;
 
-    public function getRequest()
+    public function getRequest(): RequestInterface
     {
         return $this->request;
     }
 
-    public function getResponse()
+    public function getResponse(): ResponseInterface
     {
         return $this->response;
     }
 
-    public function setResponse($response): void
+    public function setResponse(ResponseInterface $response): void
     {
         $this->response = $response;
+    }
+
+    protected function setRequest(RequestInterface $request): void
+    {
+        $this->request = $request;
     }
 
     public function __construct()
     {
         ob_start();
 
-        $this->psr17Factory = new \Nyholm\Psr7\Factory\Psr17Factory();
+        $psr17Factory = new \Nyholm\Psr7\Factory\Psr17Factory();
 
         $creator = new \Nyholm\Psr7Server\ServerRequestCreator(
-            $this->psr17Factory, // ServerRequestFactory
-                $this->psr17Factory, // UriFactory
-                $this->psr17Factory, // UploadedFileFactory
-                $this->psr17Factory  // StreamFactory
+                $psr17Factory, // ServerRequestFactory
+                $psr17Factory, // UriFactory
+                $psr17Factory, // UploadedFileFactory
+                $psr17Factory  // StreamFactory
         );
 
-        $this->request = $creator->fromGlobals();
-        $this->response = $this->psr17Factory->createResponse(418)->withBody($this->psr17Factory->createStream("I'm a teapot"));
+        $this->setRequest($creator->fromGlobals());
+        $this->setResponse($psr17Factory->createResponse(418)->withBody($psr17Factory->createStream("I'm a teapot")));
     }
 
     public function __destruct()
     {
         //clear all buffers;
-        while (ob_get_level() > 0) {
+        while (ob_get_level() > 0)
+        {
             ob_end_clean();
         }
 
