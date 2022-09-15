14b5050faf5441357075518dc89f0d19ef54ea99
TheKito <11139894+TheKito@users.noreply.github.com>
2022-09-15 09:18:27 -0300

    Update App.php

--- src/Kito/App.php
+++ src/Kito/App.php
@@ -9,6 +9,7 @@
 namespace Kito;
 
 use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\RequestInterface;
 
 /**
  * Description of App.
@@ -17,13 +18,53 @@ use Psr\Http\Message\ResponseInterface;
  */
 class App
 {
-    public function emit(ResponseInterface $response)
+
+    private $psr17Factory;
+    private $request;
+    private $response;
+
+    public function getRequest()
     {
-        while (ob_clean()) {
-            //clear all buffers;
+        return $this->request;
+    }
+
+    public function getResponse()
+    {
+        return $this->response;
+    }
+
+    public function setResponse($response): void
+    {
+        $this->response = $response;
+    }
+
+    public function __construct()
+    {
+        ob_start();
+
+        $this->psr17Factory = new \Nyholm\Psr7\Factory\Psr17Factory();
+
+        $creator = new \Nyholm\Psr7Server\ServerRequestCreator(
+                $this->psr17Factory, // ServerRequestFactory
+                $this->psr17Factory, // UriFactory
+                $this->psr17Factory, // UploadedFileFactory
+                $this->psr17Factory  // StreamFactory
+        );
+
+        $this->request = $creator->fromGlobals();
+        $this->response = $this->psr17Factory->createResponse(418)->withBody($this->psr17Factory->createStream("I'm a teapot"));
+    }
+
+    public function __destruct()
+    {
+        //clear all buffers;        
+        while (ob_get_level() > 0)
+        {
+            ob_end_clean();
         }
 
-        (new Laminas\HttpHandlerRunner\Emitter\SapiEmitter())->emit($response);
+        (new \Laminas\HttpHandlerRunner\Emitter\SapiEmitter())->emit($this->response);
         exit();
     }
+
 }
